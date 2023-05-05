bc7ad258dacf3254df17b0f551f40b4c29be4147
EST @ Q252 <erwin.schoonderwaldt@qualisys.se>
2023-05-05 14:34:25 +0200

    Renamed script (more intuitive name). Improved robustness.

--- tools/gap_fill_presets.py
+++ tools/gap_fill_presets.py
@@ -10,9 +10,9 @@ This script is especially helpful for applying relational or virtual gap filling
 of relations.
 
 The gap fill actions are compiled from a list of gap fill definitions specified by the global variable
-`list_of_definitions` in this script. The list can easily be customized by editing this variable.
+`list_of_presets` in this script. The list can easily be customized by editing this variable.
 
-The list items in list_of_definitions are formatted as a dictionary with the following key-value combinations:
+The list items in list_of_presets are formatted as a dictionary with the following key-value combinations:
   display_name: Name of the definition appearing in the submenu
   target: Name of the trajectory to be filled
   method: gap fill method, corresponding to the `algorithm` variable in the QTM Common API `fill_trajectory` method
@@ -36,14 +36,6 @@ import importlib
 
 import qtm
 
-import tools.helpers.tools
-import tools.helpers.traj
-
-importlib.reload(tools.helpers.tools)
-importlib.reload(tools.helpers.traj)
-
-from tools.helpers.tools import add_menu_item, add_command
-
 import qtm.data.object.trajectory as traj
 import qtm.data.series._3d as data_3d
 
@@ -53,15 +45,15 @@ import qtm.utilities.color as clr
 
 import copy
 
-# list_of_definitions = [\
+# list_of_presets = [\
     # {"display_name": "WaistBack (polynomial)", "target": "WaistBack", "method": "polynomial"},\
     # {"display_name": "WaistBack (kinematic)", "target": "WaistBack", "method": "kinematic"},\
     # {"display_name": "WaistLFront (rel): WaistBack-WaistRFront-WaistL", "target": "WaistLFront", "method": "relational", "settings": {"origin": "WaistBack", "line": "WaistRFront", "plane": "WaistL"}},\
     # {"display_name": "WaistRFront (rel): WaistBack-WaistLFront-WaistR", "target": "WaistRFront", "method": "relational", "settings": {"origin": "WaistBack", "line": "WaistLFront", "plane": "WaistR"}}\
     # ]
 
-# Edit the list of definitions for specifying the gap fill actions
-list_of_definitions = [\
+# Edit the list of presets defining the gap fill actions
+list_of_presets = [\
     {"display_name": "Q_WaistBack (polynomial)", "target": "Q_WaistBack", "method": "polynomial"},\
     {"display_name": "Q_WaistBack (kinematic)", "target": "Q_WaistBack", "method": "kinematic"},\
     {"display_name": "Q_WaistLFront (rel): WaistBack-WaistRFront-WaistL", "target": "Q_WaistLFront", "method": "relational", "settings": {"origin": "Q_WaistBack", "line": "Q_WaistRFront", "plane": "Q_WaistL"}},\
@@ -71,9 +63,18 @@ list_of_definitions = [\
 
 def _gap_fill_def(def_id):
     """Function for parsing and applying gap fill definitions"""
-    #find trakectory ID of target
-    gf_def = list_of_definitions[def_id]
-    id_target = traj.find_trajectory(gf_def["target"])
+    #find trajectory ID of target
+    gf_def = list_of_presets[def_id]
+    marker_name = gf_def["target"]
+    try:
+        id_target = traj.find_trajectory(marker_name)
+    except:
+        trm.write(f"No measurement.")
+        return
+    
+    if id_target == None:
+        trm.write(f"Target trajectory {marker_name} not found in measurement.")
+        return
     
     #find gap ranges:
     gap_ranges = data_3d.get_gap_ranges(id_target)
@@ -86,13 +87,28 @@ def _gap_fill_def(def_id):
         settings = copy.deepcopy(gf_def["settings"])
         if "origin" in settings.keys():
             marker_name = settings["origin"]
-            settings["origin"] = traj.find_trajectory(marker_name)
+            id_orig =  traj.find_trajectory(marker_name)
+            if id_orig == None:
+                trm.write(f"Origin trajectory {marker_name} not found in measurement.")
+                return
+            else:
+                settings["origin"] = id_orig
         if "line" in settings.keys():
             marker_name = settings["line"]
-            settings["line"] = traj.find_trajectory(marker_name)
+            id_line =  traj.find_trajectory(marker_name)
+            if id_line == None:
+                trm.write(f"Line trajectory {marker_name} not found in measurement.")
+                return
+            else:
+                settings["line"] = id_line
         if "plane" in settings.keys():
             marker_name = settings["plane"]
-            settings["plane"] = traj.find_trajectory(marker_name)
+            id_plane =  traj.find_trajectory(marker_name)
+            if id_plane == None:
+                trm.write(f"Plane trajectory {marker_name} not found in measurement.")
+                return
+            else:
+                settings["plane"] = id_plane
     else:
         settings = None
     
@@ -159,15 +175,14 @@ def add_my_commands():
 
 def setup_my_menu():
     """Function for setting up the menu."""
-    my_menu_id = qtm.gui.insert_menu_submenu(None, "My menu")
-    #qtm.gui.insert_menu_button(my_menu_id, "Fill (rel) WaistLFront", "rel_fill_waistlfront")
-    gf_sub_id = qtm.gui.insert_menu_submenu(my_menu_id, "Gap fill...")
+    my_menu_id = qtm.gui.insert_menu_submenu(None, "Gap fill presets")
+    #gf_sub_id = qtm.gui.insert_menu_submenu(my_menu_id, "Gap fill...")
 
     # Add buttons for respective relations
     # Alt. use set_draw_function callback for automatic update
     #qtm.gui.insert_menu_button(rfrb_id, "Update rigid body list...", "update_rb_refine_submenu")
-    for i in range(len(list_of_definitions)):
-        disp_name = list_of_definitions[i]["display_name"]
+    for i in range(len(list_of_presets)):
+        disp_name = list_of_presets[i]["display_name"]
         command_name = "gap_fill_def_" + str(i)
         if not(any(uc == command_name for uc in qtm.gui.get_commands("user"))):
             trm.write(f"Number of gap fill definitions exceeds maximum:\n" + \
@@ -175,11 +190,15 @@ def setup_my_menu():
             break
             
         # Add button to Refine rigid body submenu with the rigid body name and associated command
-        qtm.gui.insert_menu_button(gf_sub_id, disp_name, command_name)
+        # qtm.gui.insert_menu_button(gf_sub_id, disp_name, command_name)
+        qtm.gui.insert_menu_button(my_menu_id, disp_name, command_name)
 
 
-# Initial script actions (local 'main')
-# - Add commands and set up menu
-add_my_commands()
-setup_my_menu()
+def add_menu():
+    # - Add commands and set up menu
+    add_my_commands()
+    setup_my_menu()
 
+
+if __name__ == "__main__":
+    add_menu()
